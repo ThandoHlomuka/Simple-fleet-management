d69248ccb2e6c4f4aab7208a2827e2483462a383
Thando Hlomuka <90837977+ThandoHlomuka@users.noreply.github.com>
2024-12-21 21:53:18 +0200

    Update server.js

--- Software/backend/server.js
+++ Software/backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const vehicleRoutes = require('./routes/vehicleRoutes');
+const driverRoutes = require('./routes/driverRoutes');
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/vehicles', vehicleRoutes);
+app.use('/api/drivers', driverRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
