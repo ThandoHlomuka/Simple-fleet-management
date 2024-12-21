5b269c89321535d0232140dbbd863f075b61e93e
Thando Hlomuka <90837977+ThandoHlomuka@users.noreply.github.com>
2024-12-21 21:58:43 +0200

    Update App.js

--- Software/Frontend/src/App.js
+++ Software/Frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import VehicleList from './components/VehicleList';
+import DriverList from './components/DriverList';
 
 const App = () => {
     return (
         <div>
             <h1>Fleet Management Software</h1>
             <VehicleList />
+            <DriverList/>
         </div>
     );
 };
