1196d91f5e7b154135b048a2c6912eef8569a0c8
Thando Hlomuka <90837977+ThandoHlomuka@users.noreply.github.com>
2024-12-21 22:57:05 +0200

    Update server.js

--- Software/backend/server.js
+++ Software/backend/server.js
@@ -5,17 +5,27 @@ const vehicleRoutes = require('./routes/vehicleRoutes');
 const driverRoutes = require('./routes/driverRoutes');
 const authRoutes = require('./routes/authRoutes');
 const maintenanceRoutes = require('./routes/maintenanceRoutes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/fleet_management', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/vehicles', vehicleRoutes);
 app.use('/api/drivers', driverRoutes);
 app.use('/api/maintenance', maintenanceRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
