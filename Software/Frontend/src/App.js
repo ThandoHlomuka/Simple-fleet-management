cc62d16aab805602cfc0e8087fe576b66ce1d215
Thando Hlomuka <90837977+ThandoHlomuka@users.noreply.github.com>
2024-12-21 21:43:04 +0200

    Create App.js

---
+++ Software/Frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VehicleList from './components/VehicleList';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Fleet Management Software</h1>
+            <VehicleList />
+        </div>
+    );
+};
+
+export default App;
